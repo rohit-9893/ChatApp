d68dcb1b735aee45ce5d83cccd29deef623cd156
rohit-9893 <151185530+rohit-9893@users.noreply.github.com>
2025-07-27 00:41:31 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const port=process.env.PORT || 5000
 
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://real-timechatapp-frontend.onrender.com",
     credentials:true
 }))
 app.use(express.json())
